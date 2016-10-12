164b714b0f532ca41442bde11b7718c4de4ca5f0
Ellie Strejlau <ellie.strejlau@gmail.com>
2016-10-11 23:28:28 -0400

    dsh subcommands work, but the machine doesn't want to be created. -_-

--- install.sh
+++ install.sh
@@ -8,10 +8,19 @@ exists()
 	command -v "$1" >/dev/null 2>&1
 }
 
+# Set up our dsh script as a local executable file.
+if ! exists dsh; then
+	echo "Adding dsh binary to the path."
+	PATH=$PATH:$(pwd)/bin/
+else
+	echo "DSH command exists. Continuing with installation."
+fi
+
 
 # Download dependent container images.
 if exists docker; then
 	docker pull node
+	docker pull mongo
 else
 	echo "Error: `docker` command not found. Aborting install now. Run this script again after installing Docker."
 	exit 1;
@@ -27,26 +36,17 @@ else
 fi
 
 
-# Set up our dsh script as a local executable file.
-if ! exists dsh; then
-	echo "Adding dsh binary to the path."
-	PATH=$PATH:$(pwd)/bin/
-else
-	echo "DSH command exists. Continuing with installation."
-fi
-
-
 # Set the IP route in /etc/hosts on the host machine.
 HOSTFILE="/etc/hosts"
 if ! grep --quiet "the-crispy.docker" "$HOSTFILE"; then
-	echo "Adding DNS to hosts file for the-crispy.docker."
+	echo "Adding DNS to hosts file for crispy.docker."
     sudo cp "$HOSTFILE" "$HOSTFILE".bak
-	sudo -- sh -c -e "echo '\n# Bacon is Crispy...\n192.168.99.100\tthe-crispy.docker www.the-crispy.docker' >> /etc/hosts"
+	sudo -- sh -c -e "echo '\n# Bacon is Crispy...\n192.168.99.100\tcrispy.docker www.crispy.docker' >> /etc/hosts"
 	dscacheutil -flushcache
 else
-	echo "DNS information for tribbles.docker exists in hosts file. Continuing with installation."
+	echo "DNS information for crispy.docker exists in hosts file. Continuing with installation."
 fi
 
 
 # We're done!
-echo "Installation complete! Type `dsh st` to verify that the docker machine is running."
+echo "Installation complete! Type 'dsh st' to verify that the docker machine is running."
