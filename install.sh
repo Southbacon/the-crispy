5a7955e216be35d2a250fbd225cb90e544b715b3
Ellie Roepken <ellie.roepken@gmail.com>
2016-10-11 21:25:44 -0400

    Start devops config.

---
+++ install.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+shopt -s expand_aliases
+
+
+# Function to check to see if a command exists on the computer.
+exists()
+{
+	command -v "$1" >/dev/null 2>&1
+}
+
+
+# Download dependent container images.
+if exists docker; then
+	docker pull node
+else
+	echo "Error: `docker` command not found. Aborting install now. Run this script again after installing Docker."
+	exit 1;
+fi
+
+
+# Install our box and set up the IP in /etc/hosts.
+if exists docker-machine; then
+	dsh reload
+else
+	echo "Error: `docker-machine` command not found. Aborting install now. Run this script again after installing Docker."
+	exit 1;
+fi
+
+
+# Set up our dsh script as a local executable file.
+if ! exists dsh; then
+	echo "Adding dsh binary to the path."
+	PATH=$PATH:$(pwd)/bin/
+else
+	echo "DSH command exists. Continuing with installation."
+fi
+
+
+# Set the IP route in /etc/hosts on the host machine.
+HOSTFILE="/etc/hosts"
+if ! grep --quiet "the-crispy.docker" "$HOSTFILE"; then
+	echo "Adding DNS to hosts file for the-crispy.docker."
+    sudo cp "$HOSTFILE" "$HOSTFILE".bak
+	sudo -- sh -c -e "echo '\n# Bacon is Crispy...\n192.168.99.100\tthe-crispy.docker www.the-crispy.docker' >> /etc/hosts"
+	dscacheutil -flushcache
+else
+	echo "DNS information for tribbles.docker exists in hosts file. Continuing with installation."
+fi
+
+
+# We're done!
+echo "Installation complete! Type `dsh st` to verify that the docker machine is running."
